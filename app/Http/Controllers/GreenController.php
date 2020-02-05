ad19dc31bce91d401b06617f0971fc9da1312eff
mchev <martin.chevignard@gmail.com>
2020-02-05 17:49:58 +0100

    First commit

---
+++ app/Http/Controllers/GreenController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+
+class GreenController extends Controller
+{
+
+	public function index()
+	{
+		return view('welcome');
+	}
+
+	public function scan(Request $request)
+	{
+		
+		$url = $request->url;
+
+		return response()->json([
+			'hosting' => $this->hosting($this->remove_http($url)),
+			'carbon' => $this->carbon($url),
+		]);
+	}
+
+	public function remove_http($url) {
+	   $disallowed = array('http://', 'https://');
+	   foreach($disallowed as $d) {
+	      if(strpos($url, $d) === 0) {
+	         return str_replace($d, '', $url);
+	      }
+	   }
+	   return $url;
+	}
+
+    public function hosting($url)
+    {
+
+		$endpoint = "https://api.thegreenwebfoundation.org/greencheck/" . $url;
+		$client = new Client();
+
+		$response = $client->request('GET', $endpoint);
+
+		// url will be: http://my.domain.com/test.php?key1=5&key2=ABC;
+
+		$statusCode = $response->getStatusCode();
+		return json_decode($response->getBody()->getContents());
+
+    }
+
+    public function carbon($url)
+    {
+
+		$endpoint = "https://api.websitecarbon.com/b";
+		$client = new Client();
+
+		$response = $client->request('GET', $endpoint, ['query' => [
+		    'url' => $url, 
+		]]);
+
+		// url will be: http://my.domain.com/test.php?key1=5&key2=ABC;
+
+		$statusCode = $response->getStatusCode();
+		return json_decode($response->getBody(), true);
+
+    }
+}
---
+++ resources/js/components/Scan.vue
@@ -0,0 +1,131 @@
+<template>
+
+   <div 
+    class="d-flex align-items-center flex-column justify-content-center w-100 h-100 text-white"
+    style="background-color: #1abc9c;" 
+   >
+
+        <div v-show="loading" class="spinner">
+          <div class="double-bounce1"></div>
+          <div class="double-bounce2"></div>
+        </div>
+        
+        <h2>Estimez l'empreinte carbone de votre site web.</h2>
+        
+        <form @submit.prevent="scan" class="form-inline">
+            <div class="form-group">
+                <input class="form-control form-control-lg" v-model="url" placeholder="https://" type="url" required>
+            </div>
+            <div class="form-group">
+                <button class="btn btn-primary btn-lg ml-4" type="submit">Calculer</button>
+            </div>
+        </form>
+
+        <div v-if="result" class="container">
+
+            <div class="mt-5 results row text-center">
+                <div class="col">
+                    <p>Hébergement vert</p>
+                    <span v-if="result.hosting.green">OUI</span>
+                    <span v-else>NON</span>
+                </div>
+                <div v-if="result.hosting.hostedby" class="col">
+                    Hébergé par {{ result.hosting.hostedby }}
+                </div>
+                <div class="col">
+                    <strong>{{ result.carbon.c }}g</strong> de CO2 à chaque visite.
+                </div>
+                <div class="col">
+                    Plus propre que <strong>{{ result.carbon.p }}%</strong> des sites testés.
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+    export default {
+
+        name: 'scan',
+
+        data: function () {
+            return {
+                result: null,
+                url: null,
+                loading: false
+            }
+        },
+
+        mounted() {
+            //console.log('Component mounted.')
+        },
+
+        methods: {
+
+            scan() {
+
+                this.loading = true;
+
+                axios.get('/scan?url=' + this.url).then(response => {
+                    this.result = response.data;
+                    this.loading = false;
+                });
+            }
+        } 
+    }
+
+</script>
+
+<style>
+
+.results {
+    font-size: 2rem;
+}
+
+.spinner {
+  width: 40px;
+  height: 40px;
+
+  position: relative;
+  margin: 100px auto;
+}
+
+.double-bounce1, .double-bounce2 {
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background-color: #FFF;
+  opacity: 0.6;
+  position: absolute;
+  top: 0;
+  left: 0;
+  
+  -webkit-animation: sk-bounce 2.0s infinite ease-in-out;
+  animation: sk-bounce 2.0s infinite ease-in-out;
+}
+
+.double-bounce2 {
+  -webkit-animation-delay: -1.0s;
+  animation-delay: -1.0s;
+}
+
+@-webkit-keyframes sk-bounce {
+  0%, 100% { -webkit-transform: scale(0.0) }
+  50% { -webkit-transform: scale(1.0) }
+}
+
+@keyframes sk-bounce {
+  0%, 100% { 
+    transform: scale(0.0);
+    -webkit-transform: scale(0.0);
+  } 50% { 
+    transform: scale(1.0);
+    -webkit-transform: scale(1.0);
+  }
+}
+
+</style>
+
